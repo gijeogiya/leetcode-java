219803986ff4acd097a8a4c3db9bbb4a99743c2f
Gijeong <97646078+gijeogiya@users.noreply.github.com>
2026-08-18 21:51:53 +0900

    Update Solution.java

--- week01-array-hash/TwoSum/Solution.java
+++ week01-array-hash/TwoSum/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int result[] = new int[2];
+        int[] result = new int[2];
 
         for(int i=0; i<nums.length-1; i++){
             for(int j=i+1; j<nums.length; j++){
